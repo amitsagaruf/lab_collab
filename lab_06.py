8fdfd3928162e2d4d479525446c3c073e3553a24
amitsagaruf <sagar.amit@ufl.edu>
2023-07-20 17:10:39 -0400

    Fixed encode() after Zachary wrote decode()

--- lab_06.py
+++ lab_06.py
@@ -1,7 +1,15 @@
 # Amit Sagar
 def encode(password):
+    encoded_password = ''
     print("Your password has been encoded and stored!\n")
-    return password + 33333333
+    for num in password:
+        if int(num) >= 7:
+            num = int(num) - 7
+            encoded_password += str(num)
+        else:
+            encoded_password += str(int(num) + 3)
+    return encoded_password
+
 
 
 def decode(password):  # decodes password by subtracting 3 from each number in password (Made by: Zachary Bradley)
@@ -24,7 +32,7 @@ def main():
         print("3. Quit\n")
         option = int(input("Please enter an option: "))
         if option == 1:
-            encoded_password = encode(int(input("Please enter your password to encode: ")))
+            encoded_password = encode(input("Please enter your password to encode: "))
         if option == 2:
             print(f"The encoded password is {encoded_password}, and the original"
                   f" password is {decode(encoded_password)}.\n")
@@ -32,4 +40,5 @@ def main():
             break
 
 
-main()
+if __name__ == '__main__':
+    main()

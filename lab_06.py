2f1fc6c3aab96c65f89036cf3111ebcbab6c2b88
ZachBrad11 <zach.bradley1218.2002@gmail.com>
2023-07-20 16:36:13 -0400

    Added decode() fxn and some tabs for spacing

--- lab_06.py
+++ lab_06.py
@@ -1,11 +1,15 @@
 # Amit Sagar
 def encode(password):
-    print("Your password has been encoded and stored!")
+    print("Your password has been encoded and stored!\n")
     return password + 33333333
 
 
-def decode():
-    pass
+def decode(password):  # decodes password by subtracting 3 from each number in password (Made by: Zachary Bradley)
+        dec_pass = ''
+        password = str(password)
+        for digit in password:
+            dec_pass += str(int(digit) - 3) if int(digit) >= 3 else str(int(digit) + 7)
+        return int(dec_pass)
 
 
 def main():
@@ -17,12 +21,13 @@ def main():
         print("-------------")
         print("1. Encode")
         print("2. Decode")
-        print("3. Quit")
+        print("3. Quit\n")
         option = int(input("Please enter an option: "))
         if option == 1:
             encoded_password = encode(int(input("Please enter your password to encode: ")))
         if option == 2:
-            pass
+            print(f"The encoded password is {encoded_password}, and the original"
+                  f" password is {decode(encoded_password)}.\n")
         if option == 3:
             break
 
